d13bc5a68c08709d27b3af4c65332e5e6d1ba118
kirilukVlad <ololodr2@gmail.com>
2023-06-01 20:41:21 +0300

    portfolio+

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+function currentSlides(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    
+}
