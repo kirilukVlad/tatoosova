9951833f723f383b8f9051e465b690695b6c840f
kirilukVlad <ololodr2@gmail.com>
2023-06-05 14:45:21 +0300

    bad commit

--- Js/script.js
+++ Js/script.js
@@ -4,48 +4,44 @@ let btnLeft = document.querySelector(".btn_left");
 
 // Берём слайды
 let slides = document.querySelectorAll("span");
-console.log(slides)
 
-// Объявляем переменную i 
+// Объявляем переменную i
 let i = 0;
 // Объявляем событие нажатия на кнопку вперёд
 btnRight.addEventListener("click", function () {
-    // Увеличиваем переменную i
-    ++i
-    // Условие если переменная i больше или равна количеству слайдов
-    if (i >= slides.length) {
-        // Удаляем класс block предыдущему слайду
-        slides[i-1].classList.remove("block");
-        // Присваиваем переменной i ноль
-        i = 0;
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-        console.log(i)
-    } else { // Иначе
-        // Удаляем класс block предыдущему слайду
-        slides[i-1].classList.remove("block");
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-        console.log(i)
-    }
-})
-btnLeft.addEventListener("click" , function (){
-    i--
-    if (i <= slides.length) {
-        // Удаляем класс block предыдущему слайду
-        slides[i+1].classList.remove("block");
-        // Присваиваем переменной i ноль
-        i = 0;
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-        console.log(i)
-    } else { // Иначе
-        // Удаляем класс block предыдущему слайду
-        slides[i+1].classList.remove("block");
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-        console.log(i)
-        
-    }
-
+  // Увеличиваем переменную i
+  ++i;
+  // Условие если переменная i больше или равна количеству слайдов
+  if (i >= slides.length) {
+    // Удаляем класс block предыдущему слайду
+    slides[i - 1].classList.remove("block");
+    // Присваиваем переменной i ноль
+    i = 0;
+    // Добавляем класс block следующему слайду
+    slides[i].classList.add("block");
+  } else {
+    // Иначе
+    // Удаляем класс block предыдущему слайду
+    slides[i - 1].classList.remove("block");
+    // Добавляем класс block следующему слайду
+    slides[i].classList.add("block");
+  }
+});
+// Объявляем событие нажатия на кнопку назад
+btnLeft.addEventListener("click", function () {
+  i--;
+  if (i <= slides.length) {
+    // Добавляем класс block предыдущему слайду
+    slides[i + 1].classList.remove("block");
+    // Присваиваем переменной i ноль
+    i = 0;
+    // Добавляем класс block следующему слайду
+    slides[i].classList.add("block");
+  } else {
+    // Иначе
+    // Добавняем класс block предыдущему слайду
+    slides[i + 1].classList.remove("block");
+    // Добавляем класс block следующему слайду
+    slides[i].classList.add("block");
+  }
 });
