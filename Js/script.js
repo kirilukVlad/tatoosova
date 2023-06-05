dfbaaad50fed9b4f037041f4e7c94688c6fa4ab1
kirilukVlad <ololodr2@gmail.com>
2023-06-05 11:39:59 +0300

    questions+

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,11 @@
 // Берём кнопку вперёд
-let btnRight = document.querySelector(".btnRight");
+let btnRight = document.querySelector(".btn_right");
+let btnLeft = document.querySelector(".btn_left");
 
 // Берём слайды
-let slides = document.querySelectorAll(".questions__body");
+let slides = document.querySelectorAll("span");
 console.log(slides)
+
 // Объявляем переменную i 
 let i = 0;
 // Объявляем событие нажатия на кнопку вперёд
@@ -18,10 +20,32 @@ btnRight.addEventListener("click", function () {
         i = 0;
         // Добавляем класс block следующему слайду
         slides[i].classList.add("block");
+        console.log(i)
     } else { // Иначе
         // Удаляем класс block предыдущему слайду
         slides[i-1].classList.remove("block");
         // Добавляем класс block следующему слайду
         slides[i].classList.add("block");
+        console.log(i)
     }
 })
+btnLeft.addEventListener("click" , function (){
+    i--
+    if (i <= slides.length) {
+        // Удаляем класс block предыдущему слайду
+        slides[i+1].classList.remove("block");
+        // Присваиваем переменной i ноль
+        i = 0;
+        // Добавляем класс block следующему слайду
+        slides[i].classList.add("block");
+        console.log(i)
+    } else { // Иначе
+        // Удаляем класс block предыдущему слайду
+        slides[i+1].classList.remove("block");
+        // Добавляем класс block следующему слайду
+        slides[i].classList.add("block");
+        console.log(i)
+        
+    }
+
+});
