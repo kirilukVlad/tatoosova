492819b25795ba8ebcc5612dd38055fdf70d72bc
kirilukVlad <ololodr2@gmail.com>
2023-06-13 10:31:03 +0300

    v1.0

--- Js/script.js
+++ Js/script.js
@@ -1,46 +1,32 @@
-// Берём кнопку вперёд
 let btnRight = document.querySelector(".btn_right");
 let btnLeft = document.querySelector(".btn_left");
 
-// Берём слайды
 let slides = document.querySelectorAll("span");
 
-// Объявляем переменную i 
 let i = 0;
-// Объявляем событие нажатия на кнопку вперёд
 btnRight.addEventListener("click", function () {
-    // Увеличиваем переменную i
-    ++i
-    // Условие если переменная i больше или равна количеству слайдов
-    if (i >= slides.length) {
-        // Удаляем класс block предыдущему слайду
-        slides[i-1].classList.remove("block");
-        // Присваиваем переменной i ноль
-        i = 0;
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    } else { // Иначе
-        // Удаляем класс block предыдущему слайду
-        slides[i-1].classList.remove("block");
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    }
-})
-btnLeft.addEventListener("click" , function (){
-    i--
-    if (i <= slides.length) {
-        // Удаляем класс block предыдущему слайду
-        slides[i+1].classList.remove("block");
-        // Присваиваем переменной i ноль
-        i = 0;
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    } else { // Иначе
-        // Удаляем класс block предыдущему слайду
-        slides[i+1].classList.remove("block");
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-        
-    }
-
+  ++i;
+  if (i >= slides.length) {
+    slides[i - 1].classList.remove("block");
+    i = 0;
+    slides[i].classList.add("block");
+    console.log(i)
+  } else {
+    slides[i - 1].classList.remove("block");
+    slides[i].classList.add("block");
+    console.log(i)
+  }
+});
+btnLeft.addEventListener("click", function () {
+  i--;
+  if (i <= 0) {
+    slides[i + 1].classList.remove("block");
+    i = slides.length-1
+    slides[i].classList.add("block");
+    console.log(i)
+  } else {
+    slides[i + 1].classList.remove("block");
+    slides[i].classList.add("block");
+    console.log(i)
+  }
 });
